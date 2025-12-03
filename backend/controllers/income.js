6831e39ebdfa188be81da17fb930b4a68b1ad872
Pabasara Ashen <135622609+pabasaraashen@users.noreply.github.com>
2025-12-03 21:32:05 +0530

    Update income.js

--- backend/controllers/income.js
+++ backend/controllers/income.js
@@ -45,6 +45,11 @@ exports.getIncomes = async (req, res) => {
 
 exports.deleteIncome = async (req, res) => {
     const { id } = req.params;
-    console.log(req.params);
-   
+    IncomeSchema.findByIdAndDelete(id)
+    .then((income) => {
+        res.status(200).json({message: "Income deleted successfully"})
+    })
+    .catch((error) => {
+        res.status(500).json({message: "Server Error"})
+    })
 }
