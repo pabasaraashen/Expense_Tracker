2e7b2ce1c5bbc2c55a6ecd0abad4a73ab7bfd9bf
Pabasara Ashen <135622609+pabasaraashen@users.noreply.github.com>
2025-12-03 10:43:30 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const cors = require('cors');
+const e = require('cors');
 const app = express();
 require('dotenv').config();
 
 const PORT = process.env.PORT
 
+//middleware
+app.use(express.json());
+app.use(cors());
 const server = () => {
-    console.log(`Server is running on port ${PORT}`);
-
+    app.listen(PORT, () => {
+        console.log('listening on port ',  PORT);
+    });
 }
 
 server();
