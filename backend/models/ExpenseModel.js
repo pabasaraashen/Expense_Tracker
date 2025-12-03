9d9f4f9d87e3d13453577907a5d9c95b4e7c3946
Pabasara Ashen <135622609+pabasaraashen@users.noreply.github.com>
2025-12-03 21:40:36 +0530

    Update ExpenseModel.js

--- backend/models/ExpenseModel.js
+++ backend/models/ExpenseModel.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 
-const IncomeSchema = new mongoose.Schema({
+const ExpenseSchema = new mongoose.Schema({
     title: {
         type: String,
         required: true,
@@ -37,4 +37,4 @@ const IncomeSchema = new mongoose.Schema({
     },
 
 }, { timestamps: true });
-module.exports = mongoose.model('Income', IncomeSchema);
+module.exports = mongoose.model('Expense', ExpenseSchema);
