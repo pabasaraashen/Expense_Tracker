288367c7fda1c9dcfc23c426705e8d1f2e48ef85
Pabasara Ashen <135622609+pabasaraashen@users.noreply.github.com>
2025-12-03 21:49:35 +0530

    Add expense routes to transactions API
    Introduces routes for adding, retrieving, and deleting expenses in the transactions router by importing relevant expense controller functions.

--- backend/routes/transactions.js
+++ backend/routes/transactions.js
@@ -1,4 +1,5 @@
 const { addIncome, getIncomes, deleteIncome } = require('../controllers/income');
+const { addExpense, getExpense, deleteExpense } = require('../controllers/expense');
 
 const router = require('express').Router()
 
@@ -6,5 +7,8 @@ const router = require('express').Router()
 router.post('/add-income', addIncome)
     .get('/get-incomes', getIncomes)
     .delete('/delete-income/:id', deleteIncome)
+    .post('/add-expense', addExpense)
+    .get('/get-expenses', getExpense)
+    .delete('/delete-expense/:id', deleteExpense)
 
 module.exports = router;
