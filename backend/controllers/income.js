00e591226dd6498c67721bf532287140485cc72e
Pabasara Ashen <135622609+pabasaraashen@users.noreply.github.com>
2025-12-03 19:27:06 +0530

    Add validation and error handling to addIncome
    Enhanced the addIncome controller to include input validation, error handling, and proper response messages. Also initialized the getIncomes controller with a try-catch block for future implementation.

--- backend/controllers/income.js
+++ backend/controllers/income.js
@@ -5,5 +5,36 @@ const IncomeSchema = require("../models/incomeModel")
 exports.addIncome = async (req, res) => {
     const {title, amount, date, category, description} = req.body
 
-    const income = new IncomeSchema
+    const income = new IncomeSchema({
+        title,
+        amount,
+        category,
+        description,
+        date
+    })
+
+    try {
+        //validation
+        if(!title || !amount || !date || !category || !description){
+            return res.status(400).json({message: "All fields are required"})
+        }
+        if(amount <= 0 || !amount === "number"){
+            return res.status(400).json({message: "Amount must be positive number"})
+        }
+        await income.save();
+        res.status(200).json({message: "Income added successfully"})
+    } catch (error) {
+       res.status(500).json({message: "Server Error"}) 
+    }
+
+    console.log(income)
+}
+
+
+exports.getIncomes = async (req, res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
 }
