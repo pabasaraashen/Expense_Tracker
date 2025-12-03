ffbd0250d031cf90c5a7b4ac7fc49206c936e3b8
Pabasara Ashen <135622609+pabasaraashen@users.noreply.github.com>
2025-12-03 13:30:41 +0530

    Add income model and update transactions route message

--- backend/routes/transactions.js
+++ backend/routes/transactions.js
@@ -3,7 +3,7 @@ const router = require('express').Router()
 
 
 router.get('/', (req, res) => {
-    res.send('Hello World');
+    res.send('Transactions route works!');
 })
 
 module.exports = router;
