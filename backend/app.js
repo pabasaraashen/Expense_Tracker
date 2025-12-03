4a5fb406984a2298c1b6ab9bb4d6c18321a8a308
Pabasara Ashen <135622609+pabasaraashen@users.noreply.github.com>
2025-12-03 10:44:51 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -9,6 +9,11 @@ const PORT = process.env.PORT
 //middleware
 app.use(express.json());
 app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
 const server = () => {
     app.listen(PORT, () => {
         console.log('listening on port ',  PORT);
