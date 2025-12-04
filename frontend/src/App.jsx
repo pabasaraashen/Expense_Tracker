97811ef9a34d047678dd5cc270023aec4daf34ac
Pabasara Ashen <135622609+pabasaraashen@users.noreply.github.com>
2025-12-04 11:11:37 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,9 +2,11 @@
 
 function App() {
   return (
-    <>
-      
-    </>
+    <div className="App">
+      <h1>Hello, World!</h1>
+
+      <p>This is a simple expense tracker application.</p>
+    </div>
   )
 }
 
