f980114df01c7fed9f185d8f7136001677d85393
Pabasara Ashen <135622609+pabasaraashen@users.noreply.github.com>
2025-12-04 19:47:45 +0530

    Update TransactionsList.jsx

--- frontend/src/components/TransactionsList.jsx
+++ frontend/src/components/TransactionsList.jsx
@@ -22,9 +22,11 @@ export default function TransactionsList({ items=[], onDelete, loading }){
             </div>
             <div style={{fontSize:14, color:'var(--muted)'}}>{new Date(it.date).toLocaleDateString()}</div>
           </div>
-          <div style={{display:'flex', gap:12, alignItems:'center'}}>
+          <div style={{display:'flex', gap:200, alignItems:'center'}}>
             <div style={{fontWeight:800, fontSize:18}}>${it.amount}</div>
-            {onDelete && <button onClick={()=> onDelete(it)} style={{padding:'6px 10px'}}>Delete</button>}
+              {onDelete && (
+                <button onClick={()=> onDelete(it)} style={{padding:'6px 10px', background:'#cf0606', color:'#fff', border:'none', borderRadius:8, cursor:'pointer'}}>Delete</button>
+              )}
           </div>
         </div>
       ))}
